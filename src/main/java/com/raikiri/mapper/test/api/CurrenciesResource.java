ce665ff59214da5ce17501e4ce0eec87f79841ea
Vladimir Novoseltsev <vladimir.novoseltsev@gmail.com>
2016-08-28 23:02:52 +0300

    Explicitly state supported request methods.

--- src/main/java/com/raikiri/mapper/test/api/CurrenciesResource.java
+++ src/main/java/com/raikiri/mapper/test/api/CurrenciesResource.java
@@ -3,14 +3,14 @@ package com.raikiri.mapper.test.api;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-import com.raikiri.mapper.test.dto.CurrencyDTO;
-import com.raikiri.mapper.test.model.dictionary.Currency;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raikiri.mapper.test.dao.CurrenciesRepository;
+import com.raikiri.mapper.test.dto.CurrencyDTO;
 import com.raikiri.mapper.test.dto.ListItemDTO;
 
 @RestController
@@ -23,13 +23,13 @@ public class CurrenciesResource {
     @Autowired
     private ModelMapper modelMapper;
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
     public Collection<ListItemDTO> currenciesList() {
         return currenciesRepo.findAll().stream().map(cur -> modelMapper.map(cur, ListItemDTO.class))
                 .collect(Collectors.toList());
     }
 
-    @RequestMapping("/details")
+    @RequestMapping(path = "/details", method = RequestMethod.GET)
     public Collection<CurrencyDTO> currenciesWithDetails() {
         return currenciesRepo.findAll().stream().map(cur -> modelMapper.map(cur, CurrencyDTO.class))
                 .collect(Collectors.toList());
